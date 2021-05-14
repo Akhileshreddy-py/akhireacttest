33166847719819b1b25926ab1608746a23131145
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-14 15:00:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-k9zju8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-k9zju8)
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React,{Component} from "react";
+import "./style.css";
+
+class App extends Component {  
+
+   render() {  
+      return (  
+         <div>  
+            <h2>www.Skyeagle.com</h2>  
+            <p>This websites contains the great CS tutorial.</p>  
+         </div>  
+      );  
+   }  
+}
+class Ak extends Component
+{
+  render()
+  {
+    return(<div className="ak"><h1> hello sir </h1>
+    <h1>akhi 1</h1>
+    <h2>akhi 2</h2>
+    <h3>akhi 3</h3>
+    <h4>akhi 4</h4>
+    <h5>akhi 5</h5>
+    <h6>akhi 6</h6>
+    
+    </div>);
+  }
+}
+export default Ak;  
+ 
